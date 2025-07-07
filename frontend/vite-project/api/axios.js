6731b53b7584060644d0f1d1b287d10f2057d667
Selvakumar-6 <selvesselva12165@gmail.com>
2025-07-07 08:37:01 +0530

    Update axios.js

--- frontend/vite-project/api/axios.js
+++ frontend/vite-project/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://mern-backend-ubj0.onrender.com/",
+  baseURL: "https://mern-backend-ubj0.onrender.com/api",
 });
 
 export default api;
